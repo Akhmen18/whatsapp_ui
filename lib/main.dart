d40df2a71efab3b5823dea4780dfabbbc5e7bb89
Akhil Menon <akh180599@gmail.com>
2021-04-03 18:46:32 +0530

    Add colors and home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/screen/Home.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Color(0xff075e54),
         accentColor: Color(0xff25d366),
+        tabBarTheme: TabBarTheme(labelColor: Colors.white),
+        textTheme: TextTheme(
+          //title
+          headline6: TextStyle(color: Colors.white, fontSize: 16.0),
+          bodyText2: TextStyle(color: Colors.black),
+          button: TextStyle(color: Colors.white),
+        ),
+        bottomAppBarColor: Colors.white,
       ),
+      home: Home(),
     );
   }
 }
